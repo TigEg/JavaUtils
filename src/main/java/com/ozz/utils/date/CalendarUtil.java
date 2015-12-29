ee75bc2f8250bc952fe6f4d99fbf14ed3a130271
ouzezhou <ouzezhou@jt-xxgl-ouzezhou.staff.neworiental.org>
2015-12-29 09:46:45 +0800

    设置编码方式

--- src/main/java/com/ozz/utils/date/CalendarUtil.java
+++ src/main/java/com/ozz/utils/date/CalendarUtil.java
@@ -4,16 +4,16 @@ import java.util.Calendar;
 
 public class CalendarUtil {
 
-    public static Calendar addDay(Calendar cal, int day) {
-        cal.add(Calendar.DAY_OF_MONTH, day);
-        return cal;
-    }
+  public static Calendar addDay(Calendar cal, int day) {
+    cal.add(Calendar.DAY_OF_MONTH, day);
+    return cal;
+  }
 
-    public static Calendar setTime(Calendar cal, int hourOfDay, int minute, int second) {
-        cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        cal.set(Calendar.MINUTE, minute);
-        cal.set(Calendar.SECOND, second);
-        return cal;
-    }
+  public static Calendar setTime(Calendar cal, int hourOfDay, int minute, int second) {
+    cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+    cal.set(Calendar.MINUTE, minute);
+    cal.set(Calendar.SECOND, second);
+    return cal;
+  }
 
 }
