3d5da0b4a2b12dbc51afa7cc13805d88e65581d5
ouzezhou <ouzezhou@xdf.cn>
2016-12-21 11:29:13 +0800

    csv

--- src/main/java/com/ozz/demo/csv/CSVDemo.java
+++ src/main/java/com/ozz/demo/csv/CSVDemo.java
@@ -1,9 +1,11 @@
 package com.ozz.demo.csv;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.ArrayList;
@@ -29,7 +31,7 @@ public class CSVDemo {
     CSVFormat format = CSVFormat.DEFAULT.withHeader(FILE_HEADER).withSkipHeaderRecord();
 
     // 读出数据的代码
-    try (Reader in = new FileReader(FILE_NAME)) {
+    try (Reader in = new InputStreamReader(new FileInputStream(FILE_NAME), "gbk")) {
       Iterable<CSVRecord> records = format.parse(in);
       String strID;
       String strName;
@@ -46,17 +48,22 @@ public class CSVDemo {
     final String[] FILE_HEADER = {"Id", "Name"};
     String[][] students = new String[][] {{"001", "谭振宇"}, {"002", "周杰伦"}};
 
-    // 显式地配置CSV文件的Header，然后设置跳过Header（要不然读的时候会把头也当成一条记录）
-    CSVFormat format = CSVFormat.EXCEL.withHeader(FILE_HEADER).withSkipHeaderRecord();
-
-    // 写入CSV的代码
-    try (Writer out = new FileWriter(FILE_NAME); CSVPrinter printer = new CSVPrinter(out, format)) {
-      for (String[] student : students) {
-        List<String> records = new ArrayList<>();
-        records.add(student[0]);
-        records.add(student[1]);
-        printer.printRecord(records);
-      }
+    // 显式地配置CSV文件的Header
+    CSVFormat format = CSVFormat.EXCEL.withHeader(FILE_HEADER);
+    
+    try (Writer out = new OutputStreamWriter(new FileOutputStream(FILE_NAME), "gbk");
+        CSVPrinter printer = new CSVPrinter(out, format)) {
+      // 第一行
+      String[] student = students[0];
+      printer.print(student[0]);
+      printer.print(student[1]);
+      printer.println();
+      student = students[1];
+      // 第二行
+      List<String> records = new ArrayList<>();
+      records.add(student[0]);
+      records.add(student[1]);
+      printer.printRecord(records);
     }
   }
 }
