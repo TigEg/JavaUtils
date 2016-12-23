27634d3a57bea366a17b62e07a1e3fe55a9663c3
ouzezhou <ouzezhou@xdf.cn>
2016-12-23 16:24:11 +0800

    httpclient

--- src/main/java/com/ozz/demo/httpclient/HttpclientDemo.java
+++ src/main/java/com/ozz/demo/httpclient/HttpclientDemo.java
@@ -3,18 +3,61 @@ package com.ozz.demo.httpclient;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 public class HttpclientDemo {
+  /**
+   * Get
+   */
+  public static String doGet(String uri) throws IOException {
+    try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
+      HttpGet http = new HttpGet(uri);
+      try (CloseableHttpResponse response2 = httpclient.execute(http);) {
+        HttpEntity entity = response2.getEntity();
+        String responseStr = EntityUtils.toString(entity);
+        EntityUtils.consume(entity);
+        return responseStr;
+      }
+    }
+  }
+
+  /**
+   * Post
+   */
+  public String doPost(String url, Map<String, String> params) throws IOException {
+    try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
+      HttpPost httpPost = new HttpPost(url);
+      List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+      for (String name : params.keySet()) {
+        nvps.add(new BasicNameValuePair(name, params.get(name)));
+      }
+      httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+      try (CloseableHttpResponse response = httpclient.execute(httpPost);) {
+        HttpEntity entity = response.getEntity();
+        String responseStr = EntityUtils.toString(entity);
+        EntityUtils.consume(entity);
+        return responseStr;
+      }
+    }
+  }
+
   /**
    * 上传附件
    */
@@ -28,6 +71,7 @@ public class HttpclientDemo {
       multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
       multipartEntityBuilder.setCharset(Charset.forName("UTF-8"));
       multipartEntityBuilder.addBinaryBody("file", file);
+      // multipartEntityBuilder.addBinaryBody("file2", file2);
       httppost.setEntity(multipartEntityBuilder.build());
 
       // 发送
@@ -50,7 +94,7 @@ public class HttpclientDemo {
 
   public static void main(String[] args) throws IOException {
     try {
-      new HttpclientDemo().upload("http://jwapi.uat.staff.xdf.cn/import_excel?accessToken=5e83a0b0-d9a8-44c2-b2de-1158b8866ccc&appId=90101&businessType=2",
+      new HttpclientDemo().upload("http://jwapi.dev.staff.xdf.cn:8080/import_excel?accessToken=5e83a0b0-d9a8-44c2-b2de-1158b8866ccc&appId=90101&businessType=2",
                                   new File("C:/Users/ouzezhou/Desktop/Temp/20161223/班级模板 (7).xlsx"));
     } catch (Exception e) {
       e.printStackTrace();
