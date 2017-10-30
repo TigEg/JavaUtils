a3e7151ba2241df1b8a0ae3a7a67b672e4fa6def
ouzezhou <ouzezhou@xdf.cn>
2017-10-30 16:48:46 +0800

    remove main

--- src/main/java/com/ozz/demo/encrypt/JasyptDemo.java
+++ src/main/java/com/ozz/demo/encrypt/JasyptDemo.java
@@ -16,23 +16,23 @@ public class JasyptDemo {
     return encryptor.decrypt(encryptedMessage);
   }
 
-  public static void main(String[] args) throws Exception {
-    JasyptDemo demo = new JasyptDemo();
-
-    String source = "站在云端，敲下键盘，望着通往世界另一头的那扇窗，只为做那读懂0和1的人。。";
-    System.out.println("原文：" + source);
-
-    String key = "pdg6jd6rsofdsjfo145";
-    System.out.println("密钥：" + key);
-
-    String encryptData = demo.encrypt(key, source);
-    System.out.println("加密：" + encryptData);
-
-    String decryptData = demo.decrypt(key, encryptData);
-    System.out.println("解密: " + decryptData);
-
-    if (!source.equals(decryptData)) {
-      throw new RuntimeException("解密失败...");
-    }
-  }
+//  public static void main(String[] args) throws Exception {
+//    JasyptDemo demo = new JasyptDemo();
+//
+//    String source = "站在云端，敲下键盘，望着通往世界另一头的那扇窗，只为做那读懂0和1的人。。";
+//    System.out.println("原文：" + source);
+//
+//    String key = "pdg6jd6rsofdsjfo145";
+//    System.out.println("密钥：" + key);
+//
+//    String encryptData = demo.encrypt(key, source);
+//    System.out.println("加密：" + encryptData);
+//
+//    String decryptData = demo.decrypt(key, encryptData);
+//    System.out.println("解密: " + decryptData);
+//
+//    if (!source.equals(decryptData)) {
+//      throw new RuntimeException("解密失败...");
+//    }
+//  }
 }
