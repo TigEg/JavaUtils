7ded0542c642eaefa8342e04e235cfa74f912f0c
ouzezhou <ouzezhou@xdf.cn>
2017-11-29 14:06:04 +0800

    httpclient

--- src/main/java/com/ozz/demo/httpclient/HttpclientDemo.java
+++ src/main/java/com/ozz/demo/httpclient/HttpclientDemo.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
@@ -40,8 +39,8 @@ public class HttpclientDemo {
    */
   public String doGet(String uri) throws IOException {
     try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
-      HttpGet http = new HttpGet(uri);
-      try (CloseableHttpResponse response = httpclient.execute(http);) {
+      HttpGet httpRequest = new HttpGet(uri);
+      try (CloseableHttpResponse response = httpclient.execute(httpRequest);) {
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
           HttpEntity entity = response.getEntity();
           String responseStr = EntityUtils.toString(entity);
@@ -51,6 +50,8 @@ public class HttpclientDemo {
           throw new RuntimeException(response.getStatusLine().getStatusCode() + ":"
                                      + response.getStatusLine().getReasonPhrase());
         }
+      } finally {
+        httpRequest.abort();
       }
     }
   }
@@ -60,13 +61,13 @@ public class HttpclientDemo {
    */
   public String doPost(String url, Map<String, String> params) throws IOException {
     try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
-      HttpPost httpPost = new HttpPost(url);
+      HttpPost httpRequest = new HttpPost(url);
       List<NameValuePair> nvps = new ArrayList<NameValuePair>();
       for (String name : params.keySet()) {
         nvps.add(new BasicNameValuePair(name, params.get(name)));
       }
-      httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
-      try (CloseableHttpResponse response = httpclient.execute(httpPost);) {
+      httpRequest.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+      try (CloseableHttpResponse response = httpclient.execute(httpRequest);) {
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
           HttpEntity entity = response.getEntity();
           String responseStr = EntityUtils.toString(entity);
@@ -76,6 +77,8 @@ public class HttpclientDemo {
           throw new RuntimeException(response.getStatusLine().getStatusCode() + ":"
                                      + response.getStatusLine().getReasonPhrase());
         }
+      } finally {
+        httpRequest.abort();
       }
     }
   }
@@ -86,7 +89,7 @@ public class HttpclientDemo {
   public void upload(String url, File file) throws IOException {
     try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
       // 创建请求
-      HttpPost httppost = new HttpPost(url);
+      HttpPost httpRequest = new HttpPost(url);
 
       // 添加附件
       MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
@@ -94,22 +97,25 @@ public class HttpclientDemo {
       multipartEntityBuilder.setCharset(Charset.forName("UTF-8"));
       multipartEntityBuilder.addBinaryBody("file", file);
       // multipartEntityBuilder.addBinaryBody("file2", file2);
-      httppost.setEntity(multipartEntityBuilder.build());
+      httpRequest.setEntity(multipartEntityBuilder.build());
 
       // 发送
-      HttpResponse response = httpclient.execute(httppost);
-
-      // 解析响应值
-      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-      } else {
-        throw new RuntimeException(response.getStatusLine().getStatusCode() + ":"
-                                   + response.getStatusLine().getReasonPhrase());
+      try(CloseableHttpResponse response = httpclient.execute(httpRequest);) {
+        // 解析响应值
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        } else {
+          throw new RuntimeException(response.getStatusLine().getStatusCode() + ":"
+                                     + response.getStatusLine().getReasonPhrase());
+        }
+      } finally {
+        httpRequest.abort();
       }
     }
   }
 
-  public void main(String[] args) throws IOException {
-      new HttpclientDemo().upload("http://jwapi.dev.staff.xdf.cn:8080/import_excel?accessToken=5e83a0b0-d9a8-44c2-b2de-1158b8866ccc&appId=90101&businessType=2",
-                                  new File("C:/Users/ouzezhou/Desktop/Temp/20161223/班级模板 (7).xlsx"));
+  public static void main(String[] args) throws IOException {
+//      new HttpclientDemo().upload("http://jwapi.dev.staff.xdf.cn:8080/import_excel?accessToken=5e83a0b0-d9a8-44c2-b2de-1158b8866ccc&appId=90101&businessType=2",
+//                                  new File("C:/Users/ouzezhou/Desktop/Temp/20161223/班级模板 (7).xlsx"));
+    System.out.println(new HttpclientDemo().doGet("http://www.baidu.com"));
   }
 }
