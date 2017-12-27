25e7c4571dda801045063085aa40c6bd10f6fe6b
ouzezhou <ouzezhou@xdf.cn>
2017-12-27 16:23:57 +0800

    google authenticator

---
+++ src/main/java/com/ozz/demo/security/googleauthenticator/GoogleAuthenticator.java
@@ -0,0 +1,86 @@
+package com.ozz.demo.security.googleauthenticator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+/**
+ * http://www.asaph.org/2016/04/google-authenticator-2fa-java.html
+ */
+public class GoogleAuthenticator {
+  public static String getRandomSecretKey() {
+    SecureRandom random = new SecureRandom();
+    byte[] bytes = new byte[20];
+    random.nextBytes(bytes);
+    Base32 base32 = new Base32();
+    String secretKey = base32.encodeToString(bytes);
+    // make the secret key more human-readable by lower-casing and
+    // inserting spaces between each group of 4 characters
+    return secretKey.toLowerCase().replaceAll("(.{4})(?=.{4})", "$1 ");
+  }
+
+  public static String getTOTPCode(String secretKey) {
+    String normalizedBase32Key = secretKey.replace(" ", "").toUpperCase();
+    Base32 base32 = new Base32();
+    byte[] bytes = base32.decode(normalizedBase32Key);
+    String hexKey = Hex.encodeHexString(bytes);
+    long time = (System.currentTimeMillis() / 1000) / 30;
+    String hexTime = Long.toHexString(time);
+    return TOTP.generateTOTP(hexKey, hexTime, "6");// TOTP: Time-Based One-Time Password Algorithm
+  }
+
+  public static String getGoogleAuthenticatorBarCode(String secretKey, String account,
+      String issuer) {
+    String normalizedBase32Key = secretKey.replace(" ", "").toUpperCase();
+    try {
+      return "otpauth://totp/"
+             + URLEncoder.encode(issuer + ":" + account, "UTF-8").replace("+", "%20")
+             + "?secret="
+             + URLEncoder.encode(normalizedBase32Key, "UTF-8").replace("+", "%20")
+             + "&issuer="
+             + URLEncoder.encode(issuer, "UTF-8").replace("+", "%20");
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public static void createQRCode(String barCodeData, String filePath, int height, int width) {
+    try {
+      BitMatrix matrix = new MultiFormatWriter().encode(barCodeData, BarcodeFormat.QR_CODE, width, height);
+      try (FileOutputStream out = new FileOutputStream(filePath)) {
+        MatrixToImageWriter.writeToStream(matrix, "png", out);
+      }
+    } catch (WriterException | IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void main(String[] args) {
+    // String randomSecretKey = getRandomSecretKey();
+    String secretKey = "s2z6 vuis vcfx k4ok ysg2 nkyc 4p7s 3cfp";
+    System.out.println("secretKey: " + secretKey);
+
+    String issuer = "google-test";
+    String account = "ouzezh";
+    String barCodeData = getGoogleAuthenticatorBarCode(secretKey, account, issuer);
+    System.out.println(barCodeData + ": " + barCodeData);
+
+    String filePath = "C:/Users/ouzezhou/Desktop/QRCode.png";
+    createQRCode(barCodeData, filePath, 200, 200);
+    System.out.println("save QRCode to " + filePath);
+
+    String code = getTOTPCode(secretKey);
+    System.out.println("TOTP code: " + code);
+  }
+}
