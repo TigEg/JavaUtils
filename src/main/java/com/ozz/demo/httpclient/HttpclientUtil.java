d66c637c28784a330715cb71927d722bbd00c8c6
ouzezhou <ouzezhou@xdf.cn>
2018-01-03 18:25:58 +0800

    httpclient

--- src/main/java/com/ozz/demo/httpclient/HttpclientUtil.java
+++ src/main/java/com/ozz/demo/httpclient/HttpclientUtil.java
@@ -6,7 +6,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpStatus;
@@ -23,12 +22,16 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class HttpclientDemo {
+public class HttpclientUtil {
+  private static Logger log = LoggerFactory.getLogger(HttpclientUtil.class);
+
   /**
    * set proxy
    */
-  public void setProxy(HttpRequestBase httpRequest) {
+  public static void setProxy(HttpRequestBase httpRequest) {
     HttpHost proxy = new HttpHost("127.0.0.1", 8888, "http");
     RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
     httpRequest.setConfig(config);
@@ -37,29 +40,18 @@ public class HttpclientDemo {
   /**
    * Get
    */
-  public String doGet(String uri) throws IOException {
+  public static String doGet(String uri) throws IOException {
     try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
       HttpGet httpRequest = new HttpGet(uri);
-      try (CloseableHttpResponse response = httpclient.execute(httpRequest);) {
-        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-          HttpEntity entity = response.getEntity();
-          String responseStr = EntityUtils.toString(entity);
-          EntityUtils.consume(entity);
-          return responseStr;
-        } else {
-          throw new RuntimeException(response.getStatusLine().getStatusCode() + ":"
-                                     + response.getStatusLine().getReasonPhrase());
-        }
-      } finally {
-        httpRequest.abort();
-      }
+
+      return doRequest(httpclient, httpRequest);
     }
   }
 
   /**
    * Post
    */
-  public String doPost(String url, Map<String, String> params) throws IOException {
+  public static String doPost(String url, Map<String, String> params) throws IOException {
     try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
       HttpPost httpRequest = new HttpPost(url);
       List<NameValuePair> nvps = new ArrayList<NameValuePair>();
@@ -67,26 +59,40 @@ public class HttpclientDemo {
         nvps.add(new BasicNameValuePair(name, params.get(name)));
       }
       httpRequest.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
-      try (CloseableHttpResponse response = httpclient.execute(httpRequest);) {
-        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-          HttpEntity entity = response.getEntity();
-          String responseStr = EntityUtils.toString(entity);
-          EntityUtils.consume(entity);
-          return responseStr;
-        } else {
-          throw new RuntimeException(response.getStatusLine().getStatusCode() + ":"
-                                     + response.getStatusLine().getReasonPhrase());
-        }
-      } finally {
-        httpRequest.abort();
-      }
+
+      return doRequest(httpclient, httpRequest);
     }
+  }
+
+  private static String doRequest(CloseableHttpClient httpclient, HttpRequestBase httpRequest) throws IOException {
+    try (CloseableHttpResponse response = httpclient.execute(httpRequest);) {
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        return parseResponse(response);
+      } else {
+        String mess = String.valueOf(response.getStatusLine().getStatusCode());
+        try {
+          mess += "\n"+parseResponse(response);
+        } catch (Exception e) {
+          log.error(null, e);
+        } 
+        throw new RuntimeException(mess);
+      }
+    } finally {
+      httpRequest.abort();
+    }
+  }
+
+  private static String parseResponse(CloseableHttpResponse response) throws IOException {
+    HttpEntity entity = response.getEntity();
+    String responseStr = EntityUtils.toString(entity);
+    EntityUtils.consume(entity);
+    return responseStr;
   }
 
   /**
    * 上传附件
    */
-  public void upload(String url, File file) throws IOException {
+  public static void upload(String url, File file) throws IOException {
     try (CloseableHttpClient httpclient = HttpClients.createDefault();) {
       // 创建请求
       HttpPost httpRequest = new HttpPost(url);
@@ -114,8 +120,8 @@ public class HttpclientDemo {
   }
 
   public static void main(String[] args) throws IOException {
-//      new HttpclientDemo().upload("http://jwapi.dev.staff.xdf.cn:8080/import_excel?accessToken=5e83a0b0-d9a8-44c2-b2de-1158b8866ccc&appId=90101&businessType=2",
+//      HttpclientDemo.upload("http://jwapi.dev.staff.xdf.cn:8080/import_excel?accessToken=5e83a0b0-d9a8-44c2-b2de-1158b8866ccc&appId=90101&businessType=2",
 //                                  new File("C:/Users/ouzezhou/Desktop/Temp/20161223/班级模板 (7).xlsx"));
-    System.out.println(new HttpclientDemo().doGet("http://www.baidu.com"));
+    System.out.println(HttpclientUtil.doGet("http://www.baidu.com"));
   }
 }
