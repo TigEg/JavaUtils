efe3539ac47ce458d8d3fe1aa226ac827651f536
ouzezhou <ouzezhou@xdf.cn>
2018-01-04 17:46:46 +0800

    lambda

---
+++ src/main/java/lambda/LambdaDemo.java
@@ -0,0 +1,24 @@
+package lambda;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.Test;
+
+public class LambdaDemo {
+  @Test
+  public void test() throws InterruptedException {
+    testLambda();
+    Thread.sleep(200);
+  }
+
+  private void testLambda() {
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    executorService.execute(() -> {
+      try {Thread.sleep(100);} catch (InterruptedException e) {throw new RuntimeException(e);}
+      System.out.println("test lambda 1");
+    });
+    executorService.execute(() -> {
+      System.out.println("test lambda 2");
+    });
+  }
+}
