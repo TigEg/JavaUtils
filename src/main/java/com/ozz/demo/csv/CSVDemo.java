142d83d52dbff61dfc2cbd123ed3011dd639361a
ouzezhou <ouzezhou@xdf.cn>
2018-07-31 16:42:00 +0800

    csv

--- src/main/java/com/ozz/demo/csv/CSVDemo.java
+++ src/main/java/com/ozz/demo/csv/CSVDemo.java
@@ -22,13 +22,13 @@ import com.ozz.demo.path.ResourcePathUtil;
 
 public class CSVDemo {
   private ResourcePathUtil resourcePathUtil;
-  
+
   public void main(String[] args) throws FileNotFoundException, IOException {
     new CSVDemo().writeCsv();
     new CSVDemo().readCsv();
   }
 
-  public void readCsv() throws FileNotFoundException, IOException {
+  public void readCsv() {
     final String FILE_NAME = resourcePathUtil.getProjectPath() + "/logs/student.csv";
     final String[] FILE_HEADER = {"Id", "Name"};
 
@@ -36,44 +36,55 @@ public class CSVDemo {
     CSVFormat format = CSVFormat.EXCEL.withHeader(FILE_HEADER).withSkipHeaderRecord();
 
     // 读出数据的代码
-    try (InputStream in = new FileInputStream(FILE_NAME);
-        Reader reader = new InputStreamReader(in, "gbk")) {
+    try (InputStream in = new FileInputStream(FILE_NAME); Reader reader = new InputStreamReader(in, "gbk")) {
       Iterable<CSVRecord> records = format.parse(reader);
       for (CSVRecord record : records) {
-        record.forEach(t -> {System.out.println(t + " ");});
+        record.forEach(t -> {
+          System.out.println(t + " ");
+        });
       }
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
   }
 
-  public void writeCsv() throws IOException {
-    Path path = Paths.get(resourcePathUtil.getProjectPath() + "/logs", "student.csv");
-    Files.deleteIfExists(path);
-    Files.createFile(path);
+  public void writeCsv() {
+    try {
+      Path path = Paths.get(resourcePathUtil.getProjectPath() + "/logs", "student.csv");
+      Files.deleteIfExists(path);
+      Files.createFile(path);
 
-    try (BufferedWriter out = Files.newBufferedWriter(path, Charset.forName("UTF-8"));) {
-      // 处理乱码问题
-      String bomStr = new String(new byte[] {(byte) 0xef, (byte) 0xbb, (byte) 0xbf}, "UTF-8");
-      out.write(bomStr);
+      try (BufferedWriter out = Files.newBufferedWriter(path, Charset.forName("UTF-8"));) {
+        // 处理乱码问题
+        String bomStr = new String(new byte[] {(byte) 0xef, (byte) 0xbb, (byte) 0xbf}, "UTF-8");
+        out.write(bomStr);
 
-      // 显式地配置CSV文件的Header
-      String[] header = {"Id", "Name"};
-      CSVFormat format = CSVFormat.EXCEL.withHeader(header);
+        // 显式地配置CSV文件的Header
+        String[] header = {"Id", "Name"};
+        CSVFormat format = CSVFormat.EXCEL.withHeader(header);
 
-      // 输出
-      String[][] students = new String[][] {{"001", "谭振宇"}, {"002", "周杰伦"}};
-      try (CSVPrinter printer = new CSVPrinter(out, format)) {
-        // 第一行
-        String[] student = students[0];
-        printer.print(student[0]);
-        printer.print(student[1]);
-        printer.println();
-        student = students[1];
-        // 第二行
-        List<String> records = new ArrayList<>();
-        records.add(student[0]);
-        records.add(student[1]);
-        printer.printRecord(records);
+        // 输出
+        String[][] students = new String[][] {{"001", "谭振宇"}, {"002", "周杰伦"}};
+        try (CSVPrinter printer = new CSVPrinter(out, format)) {
+          // 第一行
+          String[] student = students[0];
+          printer.print(student[0]);
+          printer.print(student[1]);
+          printer.println();
+          student = students[1];
+          // 第二行
+          List<String> records = new ArrayList<>();
+          records.add(student[0]);
+          records.add(student[1]);
+          printer.printRecord(records);
+        }
       }
+    }  catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
   }
 }
